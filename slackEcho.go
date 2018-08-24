f40b17f81f121fae76e1daeadbe69b8a9771d05f
jakevc <jvancamp@uoregon.edu>
2018-08-23 22:05:43 -0500

    update slack echo

---
+++ slackEcho.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+// handle the http response and request
+func handler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	dat := r.Form
+	echoText := strings.Join(dat["text"], "")
+
+	fmt.Fprintf(w, echoText)
+}
+
+func main() {
+	// handle request and echo
+	http.HandleFunc("/echo", handler)
+	log.Fatal(http.ListenAndServe(":5038", nil))
+}
